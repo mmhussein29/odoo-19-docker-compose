9a36ab032077f57c22ac8e9b7f08056300febb81
mmhussein29 <89431953+mmhussein29@users.noreply.github.com>
2024-10-17 02:58:43 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ DESTINATION=$1
 PORT=$2
 CHAT=$3
 # clone Odoo directory
-git clone --depth=1 https://github.com/pro-777/odoo-18-docker-compose/ $DESTINATION
+git clone --depth=1 https://github.com/mmhussein29/odoo-18-docker-compose/ $DESTINATION
 rm -rf $DESTINATION/.git
 # set permission
 mkdir -p $DESTINATION/postgresql
