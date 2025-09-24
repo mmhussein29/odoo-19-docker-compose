94ca04485ada0bb712118a372b724341b0334872
mmhussein29 <89431953+mmhussein29@users.noreply.github.com>
2025-09-24 22:03:53 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ set -e
 # and pass them as arguments to the odoo process if not present in the config file
 : ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
 : ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
-: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo18'}}}
-: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo18@2030'}}}
+: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo19'}}}
+: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='Younnis@2030'}}}
 
 # install python packages
 pip3 install pip --upgrade
